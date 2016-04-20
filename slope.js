e20be09b3079fd23d80da5de1676b784d14557ee
Alex Cheng <alexcheng@Alexs-MacBook-Pro.local>
2016-04-20 09:48:13 -0700

    properly resending with proper working code

--- slope.js
+++ slope.js
@@ -9,3 +9,15 @@ function slope(m){
         console.log("things are trending down :(")
     }
 } //can you see the changes?
+
+x1 = 10
+x2 = 12
+
+m = 1/2
+b = 0.3
+
+y1 = m * x1 + b
+y2 = m * x2 + b
+slope(x1);
+slope(x2);
+slope(m);
